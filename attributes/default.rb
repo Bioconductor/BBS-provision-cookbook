3480ed91ce28bc3d90a6a73e9b4296e19fe47357
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-12 08:30:55 -0700

    update R url

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,4 @@ default['hostname'] = {bioc_devel_builder: "linux1.bioconductor.org",
 default['time_zone'] = "America/New_York"
 default['bbs_repos'] = 'https://github.com/Bioconductor/BBS'
 default['bbs_branch'] = 'master' # FIXME change me, probably
-default['r_url'] = 'https://cran.rstudio.com/src/base-prerelease/R-latest.tar.gz'
+default['r_url'] = 'https://cran.rstudio.com/src/base-prerelease/R-beta_2016-04-10_r70465.tar.gz'
