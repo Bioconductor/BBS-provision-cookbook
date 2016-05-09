1c74ce692c41175fef6eb605c77576aec5d435b3
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-05-09 15:10:16 -0700

    use R-3.3.0 release version, bump version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,9 @@ default['hostname'] = {dev: "linux2.bioconductor.org",
 default['time_zone'] = "America/New_York"
 default['bbs_repos'] = 'https://github.com/Bioconductor/BBS'
 default['bbs_branch'] = 'feature/linux_builders_at_ub'
-default['r_url'] = {rel: 'https://cran.rstudio.com/src/base-prerelease/R-latest.tar.gz',
-  dev: 'https://cran.rstudio.com/src/base-prerelease/R-latest.tar.gz'}
-default['r_src_dir'] = {rel: 'R-rc', dev: 'R-rc'}
+default['r_url'] = {rel: 'https://cran.rstudio.com/src/base/R-3/R-3.3.0.tar.gz',
+  dev: 'https://cran.rstudio.com/src/base/R-3/R-3.3.0.tar.gz'}
+default['r_src_dir'] = {rel: 'R', dev: 'R'}
 default['root_url'] = {dev: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz",
   rel: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz"}
 default['jags_url'] = {dev: "http://iweb.dl.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.2.0.tar.gz",
