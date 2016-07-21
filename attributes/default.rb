aad9c7fd73764e31eaa796bd674b4e092b6c3872
Andrzej Oleś <andrzej.oles@gmail.com>
2016-07-22 01:03:11 +0200

    Update to R-3.3.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,9 +6,9 @@ default['desired_hostname'] = {rel: "linux2.bioconductor.org",
 default['time_zone'] = "America/New_York"
 default['bbs_repos'] = 'https://github.com/Bioconductor/BBS'
 default['bbs_branch'] = 'feature/linux_builders_at_ub'
-default['r_url'] = {rel: 'https://cran.rstudio.com/src/base/R-3/R-3.3.0.tar.gz',
-  dev: 'https://cran.rstudio.com/src/base/R-3/R-3.3.0.tar.gz'}
-default['r_src_dir'] = {rel: 'R-3.3.0', dev: 'R-3.3.0'}
+default['r_url'] = {rel: 'https://cran.rstudio.com/src/base/R-3/R-3.3.1.tar.gz',
+  dev: 'https://cran.rstudio.com/src/base/R-3/R-3.3.1.tar.gz'}
+default['r_src_dir'] = {rel: 'R-3.3.1', dev: 'R-3.3.1'}
 default['root_url'] = {dev: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz",
   rel: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz"}
 default['jags_url'] = {dev: "http://iweb.dl.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.2.0.tar.gz",
