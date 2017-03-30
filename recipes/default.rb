a21886c2205e6e3f868150ae1d048e989d3e29ab
Hervé Pagès <hpages@fredhutch.org>
2017-03-29 22:04:47 -0700

    Add libudunits2-dev (required by CRAN package udunits2) to the list of packages to install on Ubuntu.

--- recipes/default.rb
+++ recipes/default.rb
@@ -228,7 +228,7 @@ end
     libdbi-perl libdbd-mysql-perl ggobi libgtkmm-2.4-dev libssl-dev byacc
     automake libmysqlclient-dev postgresql-server-dev-all
     firefox graphviz python-pip libxml-simple-perl texlive-lang-european
-    libmpfr-dev tree python-yaml libmodule-build-perl gdb biber
+    libmpfr-dev libudunits2-dev tree python-yaml libmodule-build-perl gdb biber
 ).each do |pkg|
     package pkg do
         action :install
