ea0e68711005abe442331920c5d1285264ce6af7
Andrzej K. Oleś <andrzej.oles@gmail.com>
2017-05-29 11:34:19 +0200

    Update attributes to reflect builder configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,15 @@
+## At release time these 4 need to be modified:
+## 'bioc_version', 'r_version', 'r_url' and 'r_src_dir'
 default['bioc_version'] = {rel: '3.5', dev: '3.6'}
-default['use_r_devel'] = {rel: false, dev: true}
 default['r_version'] = {rel: '3.4', dev: '3.4'}
+default['r_url'] = {rel: 'https://cran.rstudio.com/src/base/R-3/R-3.4.0.tar.gz',
+  dev: 'https://cran.rstudio.com/src/base/R-3/R-3.4.0.tar.gz'}
+default['r_src_dir'] = {rel: 'R-3.4.0', dev: 'R-3.4.0'}
+
 default['desired_hostname'] = {rel: "malbec1", dev: "malbec2"}
 default['time_zone'] = "America/New_York"
 default['bbs_repos'] = 'https://github.com/Bioconductor/BBS'
 default['bbs_branch'] = 'master'
-default['r_url'] = {rel: 'https://cran.rstudio.com/src/base/R-3/R-3.4.0.tar.gz',
-  dev: 'https://stat.ethz.ch/R/daily/R-devel.tar.gz'}
-default['r_src_dir'] = {rel: 'R-3.4.0', dev: 'R-devel'}
 default['root_url'] = {dev: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz",
   rel: "https://root.cern.ch/download/root_v5.34.36.source.tar.gz"}
 default['jags_url'] = {dev: "https://sourceforge.net/projects/mcmc-jags/files/JAGS/4.x/Source/JAGS-4.2.0.tar.gz/download",
