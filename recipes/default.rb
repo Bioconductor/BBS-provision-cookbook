9408847799b10edaf5c1bf189489edbcbdc4481b
Hervé Pagès <hpages@fredhutch.org>
2017-09-01 16:35:17 -0700

    Add installation of Ubuntu package libgeos-dev. It is needed by CRAN package rgeos which Bioconductor package flowDensity depends on.

--- recipes/default.rb
+++ recipes/default.rb
@@ -230,7 +230,7 @@ end
     firefox graphviz python-pip libxml-simple-perl texlive-lang-european
     libmpfr-dev libudunits2-dev tree python-yaml libmodule-build-perl gdb biber
     python-sklearn python-numpy python-pandas python-h5py
-    libprotoc-dev libprotobuf-dev protobuf-compiler libapparmor-dev
+    libprotoc-dev libprotobuf-dev protobuf-compiler libapparmor-dev libgeos-dev
 ).each do |pkg|
     package pkg do
         action :install
