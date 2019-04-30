c61399e05debe903db6f37039c96b03be60459a1
Hervé Pagès <hpages@fredhutch.org>
2019-04-30 09:09:18 -0700

    Add installation of Ubuntu package ocl-icd-opencl-dev
    Needed by BioC package gpuMagic.

--- recipes/default.rb
+++ recipes/default.rb
@@ -211,7 +211,7 @@ end
     libprotoc-dev libprotobuf-dev protobuf-compiler libapparmor-dev libgeos-dev
     librdf0-dev libmagick++-dev libsasl2-dev libpoppler-cpp-devel
     texlive-pstricks texlive-pstricks-doc texlive-luatex
-    libglpk-dev libgdal-dev
+    libglpk-dev libgdal-dev ocl-icd-opencl-dev
 ).each do |pkg|
     package pkg do
         action :install
